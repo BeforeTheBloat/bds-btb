13f2c5589e6e4453c15a1467d90540a2a153f6bb
chyves <chyvescontact@gmail.com>
2024-07-30 16:39:55 -0400

    advancement!

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include "pch.h"
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    std::string input;
+
+    while (true) {
+        std::cout << "Hello World!" << std::endl;
+        std::cout << "Enter something (or press Enter to exit): ";
+        std::getline(std::cin, input);
+
+        if (input.empty()) {
+            break;
+        }
+        
+        std::cout << "You entered: " << input << std::endl;
+    }
+
     return 0;
 }
