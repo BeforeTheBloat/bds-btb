7fd6f9887c23db31bfe80fe62224d22443c09487
chyves <chyvescontact@gmail.com>
2024-07-31 12:40:39 -0400

    server.

---
+++ src/minecraft/server.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../pch.h"
+
+class Server {
+public:
+	std::string name = "bds-btb";
+	std::string version = "1.0.0";
+
+	std::string minecraftVersion = "1.1.3";
+
+	static void Init() {
+
+	};
+};
