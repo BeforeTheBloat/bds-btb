f126b774cf39c5a515a3786bb3159374ded5fe52
chyves <chyvescontact@gmail.com>
2024-08-01 10:19:57 -0400

    linux patch 3

--- src/minecraft/network/socket_abstraction.cpp
+++ src/minecraft/network/socket_abstraction.cpp
@@ -19,27 +19,6 @@
 #include <iomanip>
 #include <sstream>
 
-class SocketAbstraction {
-public:
-    SocketAbstraction();
-    ~SocketAbstraction();
-
-    bool openSocket(const std::string& ip, unsigned short port);
-    bool sendData(const std::string& data);
-    std::string receiveData(size_t bufferSize = 1024);
-    void closeSocket();
-
-    std::string GetLocalIPAddress();
-private:
-    int sockfd;
-    struct sockaddr_in servaddr;
-
-#ifdef _WIN32
-    WSADATA wsaData;
-#endif
-};
-
-// Implementation
 
 SocketAbstraction::SocketAbstraction() : sockfd(-1) {
 #ifdef _WIN32
