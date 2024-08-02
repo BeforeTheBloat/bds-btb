d529ac7f342607422c97e848f540f8871cde8efc
chyves <chyvescontact@gmail.com>
2024-08-02 12:30:10 -0400

    test raknet by changing something stuupid

--- scripts/logger.lua
+++ scripts/logger.lua
@@ -1,6 +1,4 @@
 -- My First BDS-BTB Lua Plugin
 -- made by chyves & other bds-btb contributers
 
-Logger.Info("xstormy is da neutral.");
-Logger.Error("xstormy is da worst!!");
-Logger.Success("xstormy is da best!");
+Logger.Info("changing this to test raknet");
