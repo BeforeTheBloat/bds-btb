1f3fe0ef1b272a51f52cb7532cff25913cef176f
chyves <chyvescontact@gmail.com>
2024-09-01 15:48:58 -0400

    recode start!

--- scripts/logger.lua
+++
@@ -1,4 +0,0 @@
--- My First BDS-BTB Lua Plugin
--- made by chyves & other bds-btb contributers
-
-Logger.Info("changing this to test raknet");
---
+++ src/pch.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// C/C++ Includes
+#include <algorithm>
+#include <any>
+#include <atomic>
+#include <bitset>
+#include <cstdarg>
+#include <cstring>
+#include <ctime>
+#include <cstdint>
+#include <exception>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <sstream>
+#include <string>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+// Windows API
+#include <Windows.h>
+
+// Libraries
